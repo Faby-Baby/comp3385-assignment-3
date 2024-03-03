7511c617ec1d322208e8535a453eada69c5d5ab6
Faby-Baby <fabianbertrand94@gmail.com>
2024-03-03 19:13:13 -0400

    Aesthetic changes

--- resources/views/login-form.blade.php
+++ resources/views/login-form.blade.php
@@ -1,26 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-<h1 class="text-center">Login</h1>
-    <form class="text-center" action="" method="post">
-        @csrf
-
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <div class="text-center form-group row mb-3">
-            <label for="email" class="col-form-label col-lg-2" placeholder="johndoe@example.com" required>Email</label>
-            <div class="col-auto">
-                <input type="text" name="email" id="email" class="form-control">
-            </div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-lg-6">
+            <h1 class="text-center">Login</h1>
+            <br>
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <form action="/login" method="post">
+                @csrf
+                
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="text" name="email" id="email" class="form-control" placeholder="johndoe@example.com" required>
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" name="password" placeholder="Password" id="password" required>
+                </div>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </div>
+            </form>
         </div>
-        <div class="form-group row mb-3">
-            <label for="password" class="form-label col-lg-2" required>Password</label>
-            <div class="col-lg-4">
-                <input type="password" class="form-control" name="password" placeholder="Password" id="password">
-            </div>
-            
-        </div>
-        <div class="form-group row col-lg-4">
-            <button type="submit" class="btn btn-primary">Login</button>
-        </div>
-    </form>
+    </div>
+</div>
 @endsection
